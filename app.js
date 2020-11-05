7160d5a6a0969f1a9b72b60e3b23e26a7936f780
Travis Steed <steed0293@gmail.com>
2020-11-04 21:47:13 -0800

    added chart

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var imgOne = document.getElementById('image-one');
 var imgTwo = document.getElementById('image-two');
 var imgThree = document.getElementById('image-three');
 var myList = document.getElementById('list');
+var ctx = document.getElementById('myChart').getContext('2d');
 
 // constructor
 function Product(name) {
@@ -77,6 +78,43 @@ function renderResults() {
 
 renderProduct();
 
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
 function handleClick(event) {
   var clickedProduct = event.target.alt;
   clicks++;
@@ -89,9 +127,11 @@ function handleClick(event) {
 
   renderProduct();
   if (clicks === totalClicksAllowed) {
-    myContainer.removeEventListener('click',handleClick);
+    myContainer.removeEventListener('click', handleClick);
     renderResults();
   }
 }
+
+
 
 myContainer.addEventListener('click', handleClick);
